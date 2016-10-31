2e603689bf7168fe7ad0214e4e12467a5e33af92
Etienne Cimon <etcimon@gmail.com>
2016-10-31 10:07:00 -0400

    Fix 2.072.0 regression preventing FreeListAlloc template instantiation, fixes #11

--- source/memutils/freelist.d
+++ source/memutils/freelist.d
@@ -10,6 +10,7 @@
 module memutils.freelist;
 
 import memutils.allocators;
+import memutils.memory;
 import std.algorithm : min;
 
 final class AutoFreeListAllocator(Base : Allocator) : Allocator {
@@ -18,7 +19,10 @@ final class AutoFreeListAllocator(Base : Allocator) : Allocator {
 	private {
 		enum minExponent = 3;
 		enum freeListCount = 12;
-		FreeListAlloc!Base[freeListCount] m_freeLists;
+		static if (is(Base == MallocAllocator) && __VERSION__ >= 2072)
+			FreeListMallocAllocator[freeListCount] m_freeLists;
+		else
+			FreeListAlloc!Base[freeListCount] m_freeLists;
 		Base m_baseAlloc;
 	}
 	
@@ -28,8 +32,12 @@ final class AutoFreeListAllocator(Base : Allocator) : Allocator {
 			if (!mtx) mtx = new Mutex;
 		}
 		m_baseAlloc = getAllocator!Base();
-		foreach (i; iotaTuple!freeListCount)
-			m_freeLists[i] = new FreeListAlloc!Base(nthFreeListSize!(i));
+		foreach (i; iotaTuple!freeListCount) {
+			static if (is(Base == MallocAllocator) && __VERSION__ >= 2072)
+				m_freeLists[i] = new FreeListMallocAllocator(nthFreeListSize!(i));
+			else
+				m_freeLists[i] = new FreeListAlloc!Base(nthFreeListSize!(i));
+		}
 	}
 
 	~this() {
@@ -148,6 +156,117 @@ final class FreeListAlloc(Base : Allocator) : Allocator
 		assert(elem_size >= size_t.sizeof);
 		m_elemSize = elem_size;
 		m_baseAlloc = getAllocator!Base();
+		//logTrace("Create FreeListAlloc %d", m_elemSize);
+	}
+	
+	@property size_t elementSize() const { return m_elemSize; }
+	
+	void[] alloc(size_t sz)
+	{
+		assert(sz == m_elemSize, "Invalid allocation size.");
+		return alloc();
+	}
+	
+	void[] alloc()
+	{
+		void[] mem;
+		if (m_nfree == 0) m_firstFree = null;
+		if( m_firstFree ){
+			auto slot = m_firstFree;
+			if (--m_nfree == 0)
+				m_firstFree = null;
+			else {
+				m_firstFree = slot.next;
+			}
+			mem = (cast(void*)slot)[0 .. m_elemSize];
+		} else {
+			mem = m_baseAlloc.alloc(m_elemSize);
+			//logInfo("Alloc %d bytes: alloc: %d, free: %d", SZ, s_nalloc, s_nfree);
+		}
+		version(DebugLeaks)//if (!thread_isMainThread)
+		{
+			//import std.stdio : writeln;
+			//Exception ex = new Exception("");
+			//try throw ex; catch (Exception e) { 
+			//writeln(mem.ptr, " : ", e.toString());
+			//}
+			m_owned[cast(size_t)mem.ptr] = m_elemSize;
+
+		}
+		m_nalloc++;
+		//logInfo("Alloc %d bytes: alloc: %d, free: %d", SZ, s_nalloc, s_nfree);
+		return mem;
+	}
+
+	void[] realloc(void[] mem, size_t sz)
+	{
+		version(DebugLeaks)//if (!thread_isMainThread)
+			m_owned[cast(size_t)mem.ptr] = sz;
+		assert(mem.length == m_elemSize);
+		assert(sz == m_elemSize);
+		return mem;
+	}
+
+	void free(void[] mem)
+	{
+		assert(mem.length == m_elemSize, "Memory block passed to free has wrong size.");
+		auto s = cast(FreeListSlot*)mem.ptr;
+		s.next = m_firstFree;
+
+		version(DebugLeaks)//if (!thread_isMainThread)
+			m_owned.remove(cast(size_t)mem.ptr);
+		m_firstFree = s;
+		m_nalloc--;
+		m_nfree++;
+	}
+}
+
+// workaround for 2.072.0 regression preventing FreeListAlloc template instantiation
+private 
+final class FreeListMallocAllocator
+{
+	import memutils.vector : Vector;
+	import memutils.utils : Malloc;
+	private static struct FreeListSlot { FreeListSlot* next; }
+	private {
+		immutable size_t m_elemSize;
+		MallocAllocator m_baseAlloc;
+		FreeListSlot* m_firstFree = null;
+		size_t space;
+		version(DebugLeaks) HashMap!(size_t, size_t, Malloc) m_owned;
+		size_t m_nalloc = 0;
+		size_t m_nfree = 0;
+	}
+
+	~this() {
+		import core.thread : thread_isMainThread;
+		version(DebugLeaks)//if (!thread_isMainThread)
+		{
+			if (m_owned.length > 0)
+			{
+				import std.stdio : writeln;
+				foreach(const ref size_t ptr, const ref size_t size; m_owned)
+					writeln( cast(void*)ptr, " : ", size);
+				asm { int 3; }
+			}
+		}
+		while ( m_firstFree ){
+			auto slot = m_firstFree;
+			m_firstFree = slot.next;
+			slot.next = null;
+			m_baseAlloc.free( (cast(void*)slot)[0 .. m_elemSize] );
+			m_nfree--;
+		}
+		//foreach(size_t slot; m_owned[])
+			//	m_baseAlloc.free( (cast(void*)slot)[0 .. m_elemSize]);
+	
+	}
+	
+	this(size_t elem_size)
+	{
+		assert(elem_size >= size_t.sizeof);
+		m_elemSize = elem_size;
+		m_baseAlloc = getAllocator!MallocAllocator();
 		//logTrace("Create FreeListAlloc %d", m_elemSize);
 	}
 	
